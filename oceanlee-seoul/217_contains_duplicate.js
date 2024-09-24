8c62eca5d7fd11083797076984ea930aff804add
Daeyang lee <eodid007@gmail.com>
2024-09-24 21:58:01 +0900

    217 / Contains Duplicate / Easy / 17m (개선)

--- oceanlee-seoul/217_contains_duplicate.js
+++ oceanlee-seoul/217_contains_duplicate.js
@@ -1,13 +1,32 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// var containsDuplicate = function (nums) {
+//   const sortedNums = nums.sort();
+//   for (let i = 0; i <= sortedNums.length; i++) {
+//     if (i !== sortedNums.length) {
+//       if (sortedNums[i] === sortedNums[i + 1]) return true;
+//     }
+//   }
+//   return false;
+// };
+
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-  const sortedNums = nums.sort();
-  for (let i = 0; i <= sortedNums.length; i++) {
-    if (i !== sortedNums.length) {
-      if (sortedNums[i] === sortedNums[i + 1]) return true;
+  const seen = new Set(); // 중복 확인을 위한 해시셋 초기화
+
+  for (let i = 0; i < nums.length; i++) {
+    // 현재 숫자가 이미 해시셋에 있으면 true 반환
+    if (seen.has(nums[i])) {
+      return true;
     }
+    // 현재 숫자를 해시셋에 추가
+    seen.add(nums[i]);
   }
-  return false;
+
+  return false; // 모든 요소가 고유한 경우 false 반환
 };
