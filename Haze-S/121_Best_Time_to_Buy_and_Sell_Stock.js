2a4b6d531019035f0c5332612d3832dc0cc013e1
haze <skdml20022@gmail.com>
2024-09-25 00:50:11 +0900

    121 / Best Time to Buy and Sell Stock / easy / 8m

---
+++ Haze-S/121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let profit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (min > prices[i] && prices.indexOf(min) < i) min = prices[i];
+    profit = prices[i] - min > profit ? prices[i] - min : profit;
+  }
+
+  return profit;
+};
