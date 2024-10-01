6e440c0d48ecb7fb0c9f1f1ab76cac07ade1eec5
Liz Chung <141708445+Liz-Chung@users.noreply.github.com>
2024-10-02 00:05:41 +0800

    Update 242_valid-anagram.js

--- Liz-Chung/242_valid-anagram.js
+++ Liz-Chung/242_valid-anagram.js
@@ -3,6 +3,7 @@
  * @param {string} t
  * @return {boolean}
  */
+
 var isAnagram = function(s, t) {
     return s.length === t.length && s.split('').sort().join('') === t.split('').sort().join('');
 };
