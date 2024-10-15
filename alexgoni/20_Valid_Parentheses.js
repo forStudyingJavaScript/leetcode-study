7a2c11ba98d283e0943a4ecdbbecf19ac2924361
JooKangSan <you04045@naver.com>
2024-10-15 20:36:16 +0900

    20 / Valid Parentheses / Easy / 12m/40s

--- alexgoni/20_Valid_Parentheses.js
+++ alexgoni/20_Valid_Parentheses.js
@@ -2,27 +2,21 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
-  const stack = [];
-
-  for (let i = 0; i < s.length; i++) {
-    if (stack.length === 0) {
-      stack.push(s[i]);
-      continue;
+var isValid = function(s) {   
+    const stack = [];
+    
+    for (let i = 0 ; i < s.length ; i++) {
+        if(s[i] == '('){
+            stack.push(')');
+        }else if(s[i] == '['){
+            stack.push(']');
+        }else if(s[i] == '{'){
+            stack.push('}');
+        }else{
+            if (s[i] !== stack.pop()) {
+                return false;
+            }
+        }
     }
-
-    if (s[i] === "(" || s[i] === "{" || s[i] === "[") stack.push(s[i]);
-    else if (s[i] === ")") {
-      if (stack[stack.length - 1] === "(") stack.pop();
-      else stack.push(s[i]);
-    } else if (s[i] === "}") {
-      if (stack[stack.length - 1] === "{") stack.pop();
-      else stack.push(s[i]);
-    } else if (s[i] === "]") {
-      if (stack[stack.length - 1] === "[") stack.pop();
-      else stack.push(s[i]);
-    }
-  }
-
-  return stack.length === 0;
+    return stack.length === 0;
 };
