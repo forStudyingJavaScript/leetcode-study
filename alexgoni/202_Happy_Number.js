a02a67ceffbdbdf486cad422ab2d134e4f4c6869
JooKangSan <you04045@naver.com>
2024-10-15 20:21:51 +0900

    202 / Happy Number / Easy / 30m

--- alexgoni/202_Happy_Number.js
+++ alexgoni/202_Happy_Number.js
@@ -3,18 +3,32 @@
  * @return {boolean}
  */
 var isHappy = function (n) {
-  const visited = {};
+    let sum;
+    if (n === 1 || n === 7 ) return true;
+    while (n > 9) {
+        sum = 0;
+        const strN = String(n); 
+        for (let i = 0; i < strN.length; i++) {
+            sum += strN[i] * strN[i]; 
+        }
+        n = sum;
+        if (n === 1 || n === 7 ) return true;
+    }
+    return n === 1 || n === 7;
+};
 
-  while (true) {
-    let sum = 0;
-    [...String(n)].forEach((str) => {
-      sum += Number(str) ** 2;
-    });
-
-    if (sum === 0) return true;
-    if (visited[sum]) return false;
-
-    visited[sum] = true;
-    n = sum;
-  }
+// 기존 코드
+var isHappy = function (n) {
+    let sum;
+    if (n === 1) return true;
+    while (n > 1) {
+        sum = 0;
+        const strN = String(n); 
+        for (let i = 0; i < strN.length; i++) {
+            sum += strN[i] * strN[i]; 
+        }
+        n = sum;
+        if (n === 1) return true;
+    }
+    return n === 1
 };
