3fa4be489ccec73a8a21a8cef9022f7f440b5d02
heony704 <heony704@gmail.com>
2024-10-23 01:34:30 +0900

    70 / Climbing Stairs / Easy / 18m (리팩토링)

--- heony704/70_climbing-stairs.js
+++ heony704/70_climbing-stairs.js
@@ -6,15 +6,12 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  // n이 1이면 1, 2면 2 반환
-  if (n < 3) return n;
-
   const sequence = [1, 2];
 
   for (let i = 2; i < n; i++) {
-    const next = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+    const next = sequence[i - 1] + sequence[i - 2];
     sequence.push(next);
   }
 
-  return sequence[sequence.length - 1];
+  return sequence[n - 1];
 };
