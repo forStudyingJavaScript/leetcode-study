47d0c7658dfac8eea038dec6566672a06bc517c8
haze <skdml20022@gmail.com>
2024-10-22 21:54:10 +0900

    48 / Rotate Image / medium / 13m 42s(수정)

--- Haze-S/48_Rotate_Image.js
+++ Haze-S/48_Rotate_Image.js
@@ -3,19 +3,17 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function (matrix) {
-  for (let i = 0; i < matrix[0].length; i++) {
-    for (let j = 0; j < matrix[i][0].length; j++) {
-      const tmp = matrix[i][j];
+  let n = matrix.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      let tmp = matrix[i][j];
       matrix[i][j] = matrix[j][i];
       matrix[j][i] = tmp;
     }
   }
-};
 
-console.log(
-  rotate([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ])
-);
+  for (row of matrix) {
+    row.reverse();
+  }
+};
