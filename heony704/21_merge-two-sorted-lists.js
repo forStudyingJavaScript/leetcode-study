79e43f30331caaa9e22100163015acbefe59e39c
heony704 <heony704@gmail.com>
2024-10-22 22:37:03 +0900

    21 / Merge Two Sorted Lists / Easy / 40m (리팩토링)

--- heony704/21_merge-two-sorted-lists.js
+++ heony704/21_merge-two-sorted-lists.js
@@ -13,6 +13,36 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+// 리팩토링
+var mergeTwoLists = function (list1, list2) {
+  // 리스트가 비어있는 경우
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  // 초기화
+  let mergedList = new ListNode(-1);
+  let currentNode = mergedList;
+
+  // 두 리스트의 노드를 비교하며 병합
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      currentNode.next = list1;
+      list1 = list1.next;
+    } else {
+      currentNode.next = list2;
+      list2 = list2.next;
+    }
+    currentNode = currentNode.next;
+  }
+
+  // 남은 노드 연결
+  currentNode.next = list1 || list2;
+
+  // 첫 노드(-1) 빼고 반환
+  return mergedList.next;
+};
+
+// 처음 풀이
 var mergeTwoLists = function (list1, list2) {
   // 리스트 값 없을 때 처리
   if (!list1 || !list2) return list1 ?? list2;
