4c9cfdd49b14ed89b1a87ad20225af3ed8d19bce
JooKangSan <you04045@naver.com>
2024-10-22 20:58:46 +0900

    48 / Rotate Image / Easy / 7m40s

--- JooKangsan/21_Merge_Two_Sorted_Lists.js
+++ JooKangsan/21_Merge_Two_Sorted_Lists.js
@@ -13,7 +13,7 @@
 var mergeTwoLists = function (list1, list2) {
   if (list1 == null) return list2;
   if (list2 == null) return list1;
-  if (list1.val < list2.val) {
+  if (list1.val <= list2.val) {
     list1.next = mergeTwoLists(list1.next, list2);
     return list1;
   } else {
