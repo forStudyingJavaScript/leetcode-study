df48108d849672c64eb72af705caf92b0c37e757
oh-chaeyeon <chaebread555@gmail.com>
2024-11-05 21:47:19 +0900

    110 / Balanced Binary Tree / Easy / 16m 44s

---
+++ oh-chaeyeon/110_Balanced_Binary_Tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  const checkHeight = (node) => {
+    if (!node) return 0;
+
+    const leftHeight = checkHeight(node.left);
+    if (leftHeight === -1) return -1;
+
+    const rightHeight = checkHeight(node.right);
+    if (rightHeight === -1) return -1;
+
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  };
+
+  return checkHeight(root) !== -1;
+};
+
+// Time taken : 16m 44s
+// Runtime : 1ms
+// Memory : 53.60MB
