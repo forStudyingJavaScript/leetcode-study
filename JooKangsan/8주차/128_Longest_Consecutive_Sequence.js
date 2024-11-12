ebf2de557d299319372784a8a54b828387e8a506
JooKangSan <you04045@naver.com>
2024-11-12 20:30:56 +0900

    128 / Longest Consecutive Sequence / medium / 26m40s

--- JooKangsan/8주차/128_Longest_Consecutive_Sequence.js
+++ JooKangsan/8주차/128_Longest_Consecutive_Sequence.js
@@ -2,19 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
+function longestConsecutive(nums) {
   const numSet = new Set(nums);
-let longestStreak = 0;
-for (const num of numSet) {
-if (!numSet.has(num - 1)) {
-  let currentNum = num;
-  let currentStreak = 1;
-  while (numSet.has(currentNum + 1)) {
-    currentNum += 1;
-    currentStreak += 1;
+  let longestStreak = 0;
+    for (let i = 0; i < numSet.length; i++) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      while (numSet.has(currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
   }
-  longestStreak = Math.max(longestStreak, currentStreak);
+
+  return longestStreak;
 }
-}
-return longestStreak;
-};
