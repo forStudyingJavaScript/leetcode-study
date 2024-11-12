50658807d993cf6c1e1b926c17e35b9ed0ecd163
JooKangSan <you04045@naver.com>
2024-11-12 20:07:01 +0900

    fix: 주차별로 분류

--- JooKangsan/66_Plus_One.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number[]} digits
- * @return {number[]}
- */
-var plusOne = function (digits) {
-  const join = BigInt(digits.join("")) + 1n;
-  return join.toString().split("").map(Number);
-};
