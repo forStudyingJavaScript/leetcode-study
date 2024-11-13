3a1fae2ab58631d1c5890392e1402d38894c072c
JooKangSan <you04045@naver.com>
2024-11-13 16:22:49 +0900

    136 / Single Number / easy / 3m30s

--- JooKangsan/8주차/128_Longest_Consecutive_Sequence.js
+++ JooKangsan/8주차/128_Longest_Consecutive_Sequence.js
@@ -1,16 +1,20 @@
-function longestConsecutive(nums) {
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
   const numSet = new Set(nums);
-  let longestStreak = 0;
-  for (const num of numSet) {
-    if (!numSet.has(num - 1)) {
-      let currentNum = num;
-      let currentStreak = 1;
-      while (numSet.has(currentNum + 1)) {
-        currentNum += 1;
-        currentStreak += 1;
-      }
-      longestStreak = Math.max(longestStreak, currentStreak);
-    }
+let longestStreak = 0;
+for (const num of numSet) {
+if (!numSet.has(num - 1)) {
+  let currentNum = num;
+  let currentStreak = 1;
+  while (numSet.has(currentNum + 1)) {
+    currentNum += 1;
+    currentStreak += 1;
   }
-  return longestStreak;
+  longestStreak = Math.max(longestStreak, currentStreak);
 }
+}
+return longestStreak;
+};
