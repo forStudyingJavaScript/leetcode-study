872e53e718703e87ac77d2e3630f737b6687078f
JooKangSan <you04045@naver.com>
2024-11-13 17:41:03 +0900

    208 / Implement Trie (Prefix Tree) / medium

--- JooKangsan/7주차/155_Min_Stack.js
+++ JooKangsan/7주차/155_Min_Stack.js
@@ -23,11 +23,11 @@ class MinStack {
   }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(val)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
+/** 
+* Your MinStack object will be instantiated and called as such:
+* var obj = new MinStack()
+* obj.push(val)
+* obj.pop()
+* var param_3 = obj.top()
+* var param_4 = obj.getMin()
+*/
